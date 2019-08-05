e78c9bb0bf63f1a45e64587aa3b14e9680bf088c
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-08-05 12:52:58 +0900

    Ask stupid coach anything

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask end
+
+  def answer
+    @answer = params[:question]
+    answer_clean = @answer.strip.downcase
+    @response =
+      if answer_clean.include?('i am going to work')
+        'Great!'
+      elsif answer_clean.include?('?')
+        'Silly question, get dressed and go to work!'
+      else
+        'I don\'t care, get dressed and go to work!'
+      end
+  end
+end
